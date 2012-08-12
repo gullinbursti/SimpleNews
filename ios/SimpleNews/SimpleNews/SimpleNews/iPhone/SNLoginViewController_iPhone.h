28de18ef3b065cf292b1f8b05e6dc8cab8be8728
Matt Holcombe (gullinbursti) <github@gullinbursti-creations.com>
2012-08-12 00:13:14 -0700

    facebook open graph

---
+++ ios/SimpleNews/SimpleNews/SimpleNews/iPhone/SNLoginViewController_iPhone.h
@@ -0,0 +1,13 @@
+//
+//  SNLoginViewController_iPhone.h
+//  SimpleNews
+//
+//  Created by Matthew Holcombe on 08.11.12.
+//  Copyright (c) 2012 Sparkle Mountain, LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SNLoginViewController_iPhone : UIViewController
+
+@end
